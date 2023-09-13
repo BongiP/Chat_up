80fe13bdc3924e5dafe864c501c58920fb81f880
BongiP <bongipxulu@gmail.com>
2023-09-13 14:10:31 +0200

    merging local and remote repos
    Signed-off-by: BongiP <bongipxulu@gmail.com>

---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {getFirestore} from '@firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASN4oD1G6nYprBjRjipeYGfBipuQe5d5E",
+  authDomain: "chat-up-cbd85.firebaseapp.com",
+  projectId: "chat-up-cbd85",
+  storageBucket: "chat-up-cbd85.appspot.com",
+  messagingSenderId: "20968043930",
+  appId: "1:20968043930:web:0f2dcc6536a1cf491ab623",
+  measurementId: "G-5Z418QG2PQ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
